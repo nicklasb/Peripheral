60572d8fe4cd6d49d08cfc49b07006c48ca366d3
Nicklas Börjesson <nicklas@optimalbpm.se>
2022-05-16 00:53:58 +0200

    Implement status reporting

--- src/sdp_task.c
+++ src/sdp_task.c
@@ -2,6 +2,7 @@
 #include <string.h>
 
 #include "sdp.h"
+#include "sdp_helpers.h"
 #include "sdp_task.h"
 
 /**
@@ -46,18 +47,68 @@ void do_on_priority(struct work_queue_item *work_item)
     }
 }
 
+char *get_state(void)
+{
+    // TODO: Will a peer have other states? As "shuttingdown", "rebooting", "savingpower" or similar?
+    char *state = NULL;
+    asprintf(&state, "running");
+    return state;
+}
+
+char *get_memory(void)
+{
+
+    char *memory_str = NULL;
+    asprintf(&memory_str, "%i", heap_caps_get_free_size(MALLOC_CAP_8BIT));
+    return memory_str;
+}
+
+char *get_uptime(void)
+{
+
+    int64_t uptime = esp_timer_get_time();
+
+    char *uptime_str = NULL;
+    asprintf(&uptime_str, "%lld", uptime);
+    return uptime_str;
+}
+
+void make_status_message(uint8_t **message, int *datalength)
+{
+
+    *datalength = 0;
+
+    add_to_message(message, datalength, get_state());
+    add_to_message(message, datalength, get_uptime());
+    add_to_message(message, datalength, get_memory());
+}
+
 void do_on_work(struct work_queue_item *queue_item)
 {
     ESP_LOGI(log_prefix, "In do_on_work task on the peripheral, got a message:\n");
-    for (int i = 0; i < queue_item->partcount; i++) {
+    for (int i = 0; i < queue_item->partcount; i++)
+    {
         ESP_LOGI(log_prefix, "Message part %i: \"%s\"", i, queue_item->parts[i]);
     }
- 
+
+    uint8_t *reply_data = NULL;
+    int reply_length = 0;
+    if (strcmp((char *)(queue_item->parts[0]), "status") == 0)
+    {
+        ESP_LOGI(log_prefix, "Match");
+        make_status_message(&reply_data, &reply_length);
+    }
+
     /* Note that the worker task is run on Core 1 (APP) as upposed to all the other callbacks. */
     ESP_LOGI(log_prefix, "In do_on_work task, responding to the controller. Conversation id = %u", queue_item->conversation_id);
-    char reply_data[17] = "response\0testdata";
-
-    sdp_reply(*queue_item, DATA, &reply_data, sizeof(reply_data));
+    if (reply_data == NULL)
+    {
+        ESP_LOGI(log_prefix, "Status empty for some reason");
+    }
+    else
+    {
+        sdp_reply(*queue_item, DATA, reply_data, reply_length);
+    }
 
     /* Always call the cleanup crew when done */
     cleanup_queue_task(queue_item);
